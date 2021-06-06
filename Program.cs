20b8334f8e584243787d91959893255ee78c754a
Martin Nefjodov <Martin.Nefjodov@agileautomations.co.uk>
2021-06-06 23:31:40 +0100

    initial commit - new console app with graphql

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace graph_ql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
